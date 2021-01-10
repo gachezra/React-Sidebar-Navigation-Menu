27b9ef4d3b991ccadc0a738df287ff51aea6a4b1
Andresrosalespinzon <andresrosalespinzon@gmail.com>
2021-01-10 22:26:55 +0100

    forgot the form and email

--- react-sidebar/src/pages/Contact.js
+++ react-sidebar/src/pages/Contact.js
@@ -5,6 +5,13 @@ function Contact() {
     <div className="contact">
       <h1>Contact</h1>
       <a href="mailto:webmaster@example.com">Jon Doe</a>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
