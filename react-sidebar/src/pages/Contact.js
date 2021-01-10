2c4e9a3bab5f68acde6abefb90759a035ae63b21
Andresrosalespinzon <andresrosalespinzon@gmail.com>
2021-01-10 22:25:22 +0100

    added the homework requeriments

--- react-sidebar/src/pages/Contact.js
+++ react-sidebar/src/pages/Contact.js
@@ -4,6 +4,7 @@ function Contact() {
   return (
     <div className="contact">
       <h1>Contact</h1>
+      <a href="mailto:webmaster@example.com">Jon Doe</a>
     </div>
   );
 }
--- react-sidebar/src/pages/Products.js
+++ react-sidebar/src/pages/Products.js
@@ -4,6 +4,13 @@ function Products() {
   return (
     <div className="products">
       <h1>Products</h1>
+      <ul>
+        <li>Filming</li>
+        <li>Editing</li>
+        <li>Directing</li>
+        <li>Photography</li>
+        <li>Design</li>
+      </ul>
     </div>
   );
 }
