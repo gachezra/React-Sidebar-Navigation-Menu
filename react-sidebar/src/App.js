4cb593b0cf15d629d96655bfcc2def6806b8a1f5
Andresrosalespinzon <andresrosalespinzon@gmail.com>
2021-01-10 21:19:01 +0100

    created each page and linked to app.js

--- react-sidebar/src/App.js
+++ react-sidebar/src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/Navbar';
+// PAGES
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
 
 function App() {
   return (
@@ -10,7 +15,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" />
+          <Route path="/" component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/products" component={Products} />
+          <Route path="/reports" component={Reports} />
         </Switch>
       </Router>
     </>
