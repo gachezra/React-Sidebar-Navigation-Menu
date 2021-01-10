0fc05a6e20e2c3edc2185e963430e8fa83c2c4fb
Andresrosalespinzon <andresrosalespinzon@gmail.com>
2021-01-10 20:01:46 +0100

    First commit

---
+++ react-sidebar/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import * as FaIcons from 'react-icons/fa';
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <FaIcons.Fabars />
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
